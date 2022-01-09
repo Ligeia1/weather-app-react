ede059c249f515f1b11e1fc3238af0d819c37015
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-09 21:32:15 +0100

    Integrated API call

---
+++ src/WeatherDisplay.js
@@ -0,0 +1,6 @@
+import React from "react";
+import WeatherSearch from "./WeatherSearch";
+
+export default function WeatherDisplay() {
+  return <div className="WeatherDisplay"></div>;
+}
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -3,39 +3,52 @@ import Weather from "./Weather";
 import axios from "axios";
 
 export default function WeatherSearch(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+    });
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
 
-  function handleResponse(response) {
-    console.log(response.data);
-  }
-
   function search() {
-    const apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    return <p>hello</p>;
   }
-  return (
-    <div className="WeatherSearch">
-      <div className="row">
-        <div className="col-4"></div>
-        <div className="col-6">
-          <form onSubmit={handleSubmit}>
-            <input type="search" placeholder="Please enter a city"></input>
-          </form>
-        </div>
-        <div className="col-2">
-          <form>
-            <input
-              className="btn btn-primary"
-              type="submit"
-              value="Search"
-            ></input>
-          </form>
+  if (weatherData.ready) {
+    return (
+      <div className="WeatherSearch">
+        <div className="row">
+          <div className="col-4"></div>
+          <div className="col-6">
+            <form onSubmit={handleSubmit}>
+              <input type="search" placeholder="Please enter a city"></input>
+            </form>
+          </div>
+          <div className="col-2">
+            <form>
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Search"
+              ></input>
+            </form>
+            <WeatherDisplay data={weatherData} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "loading...";
+  }
 }
