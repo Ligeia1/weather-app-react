a713f92a5dbeeb158504028fbb961a3d6bc4a6b7
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-11 10:20:59 +0100

    Removed infinite loop

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -5,13 +5,15 @@ import "./WeatherDisplay.css";
 export default function WeatherDisplay(props) {
   return (
     <div className="WeatherDisplay container">
-      return (
-      <WeatherSearch defaultCity="Hannover" />
-      <ul className="cityInfo">
-        <h1>Hannover</h1>
-        <li>Friday, December 17</li>
-        <li>light intensity drizzle</li>
-      </ul>
+      <div className="row">
+        <div className="col">
+          <ul className="cityInfo">
+            <h1>Hannover</h1>
+            <li>Friday, December 17</li>
+            <li>light intensity drizzle</li>
+          </ul>
+        </div>
+      </div>
       <div className="row">
         <div className="col">
           <img src="http://openweathermap.org/img/wn/09d@2x.png" /> 3°C
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -3,67 +3,27 @@ import axios from "axios";
 import WeatherDisplay from "./WeatherDisplay";
 import "./WeatherSearch.css";
 
-export default function WeatherSearch(props) {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
-  const [city, setCity] = useState(props.defaultCity);
-
-  function showWeather(response) {
+export default function WeatherSearch() {
+  function handleResponse(response) {
     console.log(response.data);
-    setReady(true);
-    setWeatherData({
-      temperature: Math.round(response.data.main.temp),
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed * 3.6,
-      city: response.data.name,
-    });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
-
-  function search() {
-    const apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
-  if (ready) {
-    return (
-      <div className="WeatherSearch container">
-        <div className="row">
-          <div className="col-4"></div>
-          <div className="col-6">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="search"
-                placeholder="Please enter a city"
-                autoFocus="on"
-                onChange={handleCityChange}
-              ></input>
-            </form>
-          </div>
-          <div className="col-2">
-            <form>
-              <input
-                className="btn btn-primary"
-                type="submit"
-                value="Search"
-              ></input>
-            </form>
-            <WeatherDisplay data={weatherData} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    search();
-    return "Loading...";
-  }
+  const apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
+  let city = "Hannover";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div className="WeatherSearch container">
+      <form>
+        <input
+          type="search"
+          placeholder="Please enter a city"
+          autoFocus="on"
+          className="form-control"
+        ></input>
+        <input className="btn btn-primary" type="submit" value="Search"></input>
+      </form>
+      <WeatherDisplay />
+    </div>
+  );
 }
