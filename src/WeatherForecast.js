1f22d0cf55722bd64ce643474effa796873537e2
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-16 19:55:51 +0100

    API integration in forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  function getForecast() {
+    const apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   return (
     <div className="WeatherForecast">
       <div className="container">
         <div className="text-center d-flex justify-content-evenly">
           <span className="forecast-day">Mon</span>
-          <span>icon</span>
+          <span className="weather-icon">icon</span>
           <span>
-            <span>max </span>|<span> min </span>
+            <span className="tempMax">max </span>|
+            <span className="tempMin"> min </span>
           </span>
         </div>
       </div>
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -19,6 +19,7 @@ export default function WeatherSearch(props) {
       description: response.data.weather[0].description,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
+      coordinates: response.data.coord,
     });
   }
 
@@ -62,7 +63,7 @@ export default function WeatherSearch(props) {
         </form>
         <WeatherDisplay info={weatherData} />
         <hr />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
         <WeatherForecast />
       </div>
     );
