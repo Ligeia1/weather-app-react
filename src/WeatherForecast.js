27742d469966dd5d3b94af8afdf95a32b28d11c3
Ligeia1 <90216871+Ligeia1@users.noreply.github.com>
2022-01-17 17:11:06 +0100

    API integration & useEffect for date update

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     console.log(response.data.daily);
@@ -25,20 +29,18 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="container">
-          <div>
-            {forecastData.map(function (dailyForecast, index) {
-              if (index < 5) {
-                return (
-                  <div key={index}>
-                    <WeatherForecastDay data={dailyForecast} />
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </div>
+        <div>
+          {forecastData.map(function (dailyForecast, index) {
+            if (index > 0 && index < 6) {
+              return (
+                <div key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,15 +1,36 @@
 import React from "react";
+import "./WeatherForecastDay.css";
 
-export default function WeatherForecastDay() {
+export default function WeatherForecastDay(props) {
+  let tempMax = Math.round(props.data.temp.max);
+  let tempMin = Math.round(props.data.temp.min);
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div className="WeatherForecastDay">
-      <div className="text-center d-flex justify-content-evenly">
-        <span className="forecast-day">Mon</span>
-        <span className="weather-icon">icon</span>
-        <span>
-          <span className="tempMax">max </span>|
-          <span className="tempMin"> min </span>
-        </span>
+      <div className="container">
+        <div className="text-center d-flex justify-content-evenly">
+          <span className="forecast-day">{day()}</span>
+          <span className="weather-icon">
+            <img
+              src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
+              alt={props.data.weather[0].description}
+              className="float-left"
+              width={50}
+            />
+          </span>
+          <span>
+            <span className="tempMax">{tempMax}°C </span>|
+            <span className="tempMin"> {tempMin}°C</span>
+          </span>
+        </div>
       </div>
     </div>
   );
